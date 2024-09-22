ffcc3c2f5514ad8dace3814534981ff5d1a0ee7a
Mohamed Riadh Sohnoun <mohamedriadh.sahnoun@cydista.com>
2024-09-22 22:44:16 +0100

    BROKER: StorageBroker

---
+++ Broker/Storages/IStorageBroker.cs
@@ -0,0 +1,6 @@
+﻿namespace QuizAI.Api.Broker.Storages
+{
+    public interface IStorageBroker
+    {
+    }
+}
--- Broker/Storages/StorageBroker.cs
+++ Broker/Storages/StorageBroker.cs
@@ -1,6 +1,27 @@
-﻿namespace QuizAI.Api.Broker.Storages
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QuizAI.Api.Broker.Storages
 {
-    public class StorageBroker
+    public partial class StorageBroker : DbContext, IStorageBroker
     {
+        private readonly ILogger<StorageBroker> Logger;
+        private readonly IConfiguration Configration;
+
+        public StorageBroker(ILogger<StorageBroker> logger, IConfiguration configration)
+        {
+            Logger = logger;
+            Configration = configration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            CreateQuizModel(modelBuilder);
+            CreateQuizTypeModel(modelBuilder);
+            CreateQuizLevelModel(modelBuilder);
+            CreateResponseModel(modelBuilder);
+        }
     }
 }
